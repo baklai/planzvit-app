95e219d3dbdbcf5cd511f5990cc7ff5206e1b3b6
Dmitrii <d.n.baklai@gmail.com>
2025-01-14 17:25:20 +0200

    Добавить генерацию цветов для графиков и обновить настройки графиков в представлении HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue';
 import { useToast } from 'primevue/usetoast';
 
 import { useStatistic } from '@/stores/api/statistics';
+import { GenerateColors } from '@/service/GenerateColors';
 import { dateToStr } from '@/service/DataFilters';
 
 const toast = useToast();
@@ -11,21 +12,30 @@ const Statistic = useStatistic();
 const statistic = ref();
 
 const departmentChart = ref();
+const departmentChartOptions = ref();
+
 const branchChart = ref();
+const branchChartOptions = ref();
+
 const departmentReportChart = ref();
+const departmentReportOptions = ref();
+
 const branchReportChart = ref();
-const chartOptions = ref();
+const branchReportChartOptions = ref();
 
 const setDepartmentChartData = data => {
   const documentStyle = getComputedStyle(document.documentElement);
+  const colors = GenerateColors(data.length);
 
   return {
     labels: data.map(({ name }) => name),
     datasets: [
       {
         type: 'bar',
-        label: 'Кількість сервісів по відділах',
-        backgroundColor: documentStyle.getPropertyValue('--p-green-500'),
+        label: 'Кількість сервісів',
+        backgroundColor: colors.map(color => color.backgroundColor),
+        borderColor: colors.map(color => color.borderColor),
+        borderWidth: 1,
         data: data.map(({ servicesCount }) => servicesCount)
       }
     ]
@@ -40,8 +50,10 @@ const setBranchChartData = data => {
     datasets: [
       {
         type: 'bar',
-        label: 'Кількість підрозділів по службах / філіях',
-        backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+        label: 'Кількість підрозділів',
+        backgroundColor: data.map(item => 'rgba(6, 182, 212, 0.2)'),
+        borderColor: data.map(item => 'rgb(6, 182, 212)'),
+        borderWidth: 1,
         data: data.map(({ subdivisionsCount }) => subdivisionsCount)
       }
     ]
@@ -50,14 +62,17 @@ const setBranchChartData = data => {
 
 const setDepartmentReportChartData = data => {
   const documentStyle = getComputedStyle(document.documentElement);
+  const colors = GenerateColors(data.length);
 
   return {
     labels: data.map(({ department }) => department),
     datasets: [
       {
         type: 'bar',
-        label: 'Кількість виконаних робіт по відділах за поточний місяць',
-        backgroundColor: documentStyle.getPropertyValue('--p-green-500'),
+        label: 'Кількість виконаних робіт',
+        backgroundColor: colors.map(color => color.backgroundColor),
+        borderColor: colors.map(color => color.borderColor),
+        borderWidth: 1,
         data: data.map(({ currentJobCount }) => currentJobCount)
       }
     ]
@@ -72,15 +87,17 @@ const setBranchReportChartData = data => {
     datasets: [
       {
         type: 'bar',
-        label: 'Кількість виконаних робіт по службам(філіям) за поточний місяць',
-        backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+        label: 'Кількість виконаних робіт',
+        backgroundColor: data.map(item => 'rgba(6, 182, 212, 0.2)'),
+        borderColor: data.map(item => 'rgb(6, 182, 212)'),
+        borderWidth: 1,
         data: data.map(({ currentJobCount }) => currentJobCount)
       }
     ]
   };
 };
 
-const setChartOptions = () => {
+const setChartOptions = titleText => {
   const documentStyle = getComputedStyle(document.documentElement);
   const textColor = documentStyle.getPropertyValue('--p-text-color');
   const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
@@ -90,11 +107,16 @@ const setChartOptions = () => {
     maintainAspectRatio: false,
     aspectRatio: 0.8,
     plugins: {
+      title: {
+        display: true,
+        text: titleText || ''
+      },
       tooltips: {
         mode: 'index',
         intersect: false
       },
       legend: {
+        display: false,
         labels: {
           color: textColor
         }
@@ -126,15 +148,24 @@ const setChartOptions = () => {
 onMounted(async () => {
   try {
     statistic.value = await Statistic.dashboard();
+
     departmentChart.value = setDepartmentChartData(statistic.value.departmentChart);
+    departmentChartOptions.value = setChartOptions('Кількість сервісів по відділах');
+
     branchChart.value = setBranchChartData(statistic.value.branchChart);
+    branchChartOptions.value = setChartOptions('Кількість підрозділів по службах (філіях)');
 
     departmentReportChart.value = setDepartmentReportChartData(
       statistic.value.departmentReportChart
     );
-    branchReportChart.value = setBranchReportChartData(statistic.value.branchReportChart);
+    departmentReportOptions.value = setChartOptions(
+      'Кількість виконаних робіт по відділах за поточний місяць'
+    );
 
-    chartOptions.value = setChartOptions();
+    branchReportChart.value = setBranchReportChartData(statistic.value.branchReportChart);
+    branchReportChartOptions.value = setChartOptions(
+      'Кількість виконаних робіт по службам (філіям) за поточний місяць'
+    );
   } catch (err) {
     toast.add({
       severity: 'warn',
@@ -220,11 +251,16 @@ onMounted(async () => {
 
     <div class="flex w-full flex-row gap-4">
       <div class="w-full p-4">
-        <Chart type="bar" :data="departmentChart" :options="chartOptions" class="min-h-[30rem]" />
+        <Chart
+          type="bar"
+          :data="departmentChart"
+          :options="departmentChartOptions"
+          class="min-h-[30rem]"
+        />
       </div>
 
       <div class="w-full p-4">
-        <Chart type="bar" :data="branchChart" :options="chartOptions" class="min-h-[30rem]" />
+        <Chart type="bar" :data="branchChart" :options="branchChartOptions" class="min-h-[30rem]" />
       </div>
     </div>
 
@@ -233,13 +269,18 @@ onMounted(async () => {
         <Chart
           type="bar"
           :data="departmentReportChart"
-          :options="chartOptions"
+          :options="departmentReportOptions"
           class="min-h-[30rem]"
         />
       </div>
 
       <div class="w-full p-4">
-        <Chart type="bar" :data="branchReportChart" :options="chartOptions" class="min-h-[30rem]" />
+        <Chart
+          type="bar"
+          :data="branchReportChart"
+          :options="branchReportChartOptions"
+          class="min-h-[30rem]"
+        />
       </div>
     </div>
   </div>
